c3d342665c749d9f1016777cb672722db9335158
Spark Cheng <spark991905@gmail.com>
2026-03-10 15:21:20 +0800

    refactor: disable LIFF initialization and directly redirect to `/sign-up`.

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 // import './style.css';
-import liff from '@line/liff';
+// import liff from '@line/liff';
 
-liff
-  .init({
-    liffId: import.meta.env.VITE_LIFF_ID
-  })
-  .then(() => {
-    window.location.href = '/sign-up';
-  })
-  .catch((error) => {
-    document.querySelector('#app').innerHTML = `
-    <h1>create-liff-app</h1>
-    <p>LIFF init failed.</p>
-    <p><code>${error}</code></p>
-    <a href="https://developers.line.biz/ja/docs/liff/" target="_blank" rel="noreferrer">
-      LIFF Documentation
-    </a>
-  `;
-  });
+window.location.href = '/sign-up';
+
+// liff
+//   .init({
+//     liffId: import.meta.env.VITE_LIFF_ID
+//   })
+//   .then(() => {
+//     window.location.href = '/sign-up';
+//   })
+//   .catch((error) => {
+//     document.querySelector('#app').innerHTML = `
+//     <h1>create-liff-app</h1>
+//     <p>LIFF init failed.</p>
+//     <p><code>${error}</code></p>
+//     <a href="https://developers.line.biz/ja/docs/liff/" target="_blank" rel="noreferrer">
+//       LIFF Documentation
+//     </a>
+//   `;
+//   });
