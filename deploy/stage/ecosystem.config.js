4bbe849ec9d722a468b392c18323009e9fcd96d3
Eric Huang <iamgold0105@gmail.com>
2026-06-11 14:38:39 +0800

    feat: add deploy files

---
+++ deploy/stage/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "supreme-salmon-liff",
+      exec_mode: "cluster",
+      instances: "2", // Or a number of instances
+      script: "./node_modules/next/dist/bin/next",
+      args: "start --port 5050",
+      cwd: "/var/www/supreme-salmon-liff/webroot",
+    },
+  ],
+};
