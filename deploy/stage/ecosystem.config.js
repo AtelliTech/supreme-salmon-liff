0a2e73be07a4582cbd282824e7d3dfcdaa0e211d
Eric Huang <iamgold0105@gmail.com>
2026-06-11 14:49:39 +0800

    fix: change listen port from 5050 to 5051

--- deploy/stage/ecosystem.config.js
+++ deploy/stage/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: "cluster",
       instances: "2", // Or a number of instances
       script: "./node_modules/next/dist/bin/next",
-      args: "start --port 5050",
+      args: "start --port 5051",
       cwd: "/var/www/supreme-salmon-liff/webroot",
     },
   ],
