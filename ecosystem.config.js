97b7b4eaacf3ee6e49c741aafc3f49b3c18df536
Eric Huang <iamgold0105@gmail.com>
2026-06-15 14:56:35 +0800

    feat: change deploy mode for windows

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,24 @@ module.exports = {
   apps: [
     {
       name: "supreme-salmon-liff",
+
+      // Next.js standalone output
+      script: "server.js",
+      cwd: "C:\\sites\\supreme-salmon-liff\\current",
+
       exec_mode: "cluster",
-      instances: "2", // Or a number of instances
-      script: "./node_modules/next/dist/bin/next",
-      args: "start --port 5050",
-      cwd: "/var/www/supreme-salmon-liff/webroot",
+      instances: 2,
+
+      env: {
+        NODE_ENV: "production",
+        PORT: "3000",
+      },
+
+      max_memory_restart: "512M",
+
+      error_file: "C:\\deploy\\logs\\supreme-salmon-liff-error.log",
+      out_file: "C:\\deploy\\logs\\supreme-salmon-liff-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
   ],
 };
